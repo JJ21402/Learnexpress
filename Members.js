67312057f834b30b04400e08ee5baaeb762c8a43
JJ <pasin.pnz@mail.kmutt.ac.th>
2021-02-02 14:44:22 +0700

    CRUD app using express and postman

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id:1,
+        name:'John DOe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id:2,
+        name:'bob',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id:3,
+        name:'sho',
+        email: 'sho@gmail.com',
+        status: 'active'
+    },
+]
+
+module.exports = members;
